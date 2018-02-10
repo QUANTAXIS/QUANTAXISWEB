2310800bee64f64bb7ac1329821836cf61192b03
yutiansut <yutiansut@qq.com>
2018-02-10 21:24:54 +0800

    #web

--- quantaxisweb/app.py
+++ quantaxisweb/app.py
@@ -1,12 +1,11 @@
-#coding:utf-8
+# coding:utf-8
 
 import QUANTAXIS as QA
 import tornado.httpserver
 import tornado.ioloop
 import tornado.options
 import tornado.web
+import tornado.websocket
+
 from tornado.options import define
-define("port", default=8000, help="run on the given port", type=int)
-
-
-
+define("port", default=4000, help="run on the given port", type=int)
