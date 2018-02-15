76b7bf336fb38a0b7d9255e08b0f151ba5600d66
yutiansut <yutiansut@qq.com>
2018-02-15 17:13:29 +0800

    WEBSOCKET!!

--- quantaxisweb/quotation/handles.py
+++ quantaxisweb/quotation/handles.py
@@ -1,9 +1,10 @@
 # coding:utf-8
-
+import os
 import tornado
 from tornado.web import Application, RequestHandler, authenticated
 from tornado.websocket import WebSocketHandler
-
+import QUANTAXIS as QA
+import time
 """
 要实现2个api
 
@@ -14,6 +15,9 @@ from tornado.websocket import WebSocketHandler
 """
 
 
+class INDEX(RequestHandler):
+    def get(self):
+        self.render("index.html")
 
 
 class REALTIME(WebSocketHandler):
@@ -25,3 +29,32 @@ class REALTIME(WebSocketHandler):
 
     def on_close(self):
         print('connection close')
+
+
+class SIMULATED(WebSocketHandler):
+    def open(self):
+        self.write_message('start')
+
+    def on_message(self, message):
+        if len(str(message))==6:
+            data=QA.QA_util_to_json_from_pandas(QA.QA_fetch_stock_day(message,'2010-01-01','2017-12-31','pd'))
+            for item in data:
+                self.write_message(item)
+                time.sleep(1)
+        
+
+    def on_close(self):
+        print('connection close')
+
+
+if __name__ == '__main__':
+    app = Application(
+        handlers=[
+            (r"/", INDEX),
+            (r"/realtime", REALTIME),
+            (r"/simulate", SIMULATED)
+        ],
+        debug=True
+    )
+    app.listen(8010)
+    tornado.ioloop.IOLoop.instance().start()
