c515cedf686adbc5bb8e6a498d88373a42148d84
yutiansut <yutiansut@qq.com>
2018-02-16 22:23:15 +0800

    #update stock_min handler

--- qabackend/data/handles.py
+++ qabackend/data/handles.py
@@ -7,7 +7,7 @@ import tornado
 from tornado.web import Application, RequestHandler, authenticated
 from tornado.websocket import WebSocketHandler
 
-from QUANTAXIS.QAFetch.QAQuery import QA_fetch_stock_day
+from QUANTAXIS.QAFetch.QAQuery import QA_fetch_stock_day, QA_fetch_stock_min
 from QUANTAXIS.QAFetch.QAQuery_Advance import QA_fetch_stock_day_adv
 from QUANTAXIS.QAUtil.QATransform import QA_util_to_json_from_pandas
 
@@ -30,14 +30,32 @@ class StockdayHandler(BaseHandler):
         data = QA_util_to_json_from_pandas(
             QA_fetch_stock_day(code, start, end, format='pd'))
 
-        self.write({'result':data})
+        self.write({'result': data})
+
+
+class StockminHandler(BaseHandler):
+    def get(self):
+        """
+        采用了get_arguents来获取参数
+        默认参数: code-->000001 start-->2017-01-01 09:00:00 end-->now
+
+        """
+        code = self.get_argument('code', default='000001')
+        start = self.get_argument('start', default='2017-01-01 09:00:00')
+        end = self.get_argument('end', default=str(datetime.datetime.now()))
+        frequence = self.get_argument('frequence', default='1min')
+        data = QA_util_to_json_from_pandas(
+            QA_fetch_stock_min(code, start, end, format='pd', frequence=frequence))
+
+        self.write({'result': data})
 
 
 if __name__ == "__main__":
 
     app = Application(
         handlers=[
-            (r"/stock/day", StockdayHandler)
+            (r"/stock/day", StockdayHandler),
+            (r"/stock/min", StockminHandler)
         ],
         debug=True
     )
