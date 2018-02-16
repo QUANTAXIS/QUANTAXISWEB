c21a046c233065769b6a07806255aa1e7df1b2f4
yutiansut <yutiansut@qq.com>
2018-02-16 22:52:56 +0800

    #update for a total website

--- qabackend/app.py
+++ qabackend/app.py
@@ -1,13 +1,28 @@
 # coding:utf-8
 
-import tornado.httpserver
-import tornado.ioloop
-import tornado.options
-import tornado.web
-import tornado.websocket
-from tornado.options import define
+import tornado
+from tornado.web import Application, RequestHandler, authenticated
+from data.handles import StockdayHandler, StockminHandler
+from quotation.handles import RealtimeSocketHandler, SimulateSocketHandler
+from user.handles import SigninHandler, SignupHandler
 
-import QUANTAXIS as QA
-from data.handles import StockdayHandler,StockminHandler
-from quotation.handles import RealtimeSocketHandler,SimulateSocketHandler
-from user.handles import *
+class INDEX(RequestHandler):
+    def get(self):
+        self.render("index.html")
+
+
+if __name__ == '__main__':
+    app = Application(
+        handlers=[
+            (r"/", INDEX),
+            (r"/stock/day", StockdayHandler),
+            (r"/stock/min", StockminHandler),
+            (r"/user/signin", SigninHandler),
+            (r"/user/signup", SignupHandler),
+            (r"/realtime", RealtimeSocketHandler),
+            (r"/simulate", SimulateSocketHandler)
+        ],
+        debug=True
+    )
+    app.listen(8010)
+    tornado.ioloop.IOLoop.instance().start()
--- qabackend/user/handles.py
+++ qabackend/user/handles.py
@@ -7,26 +7,40 @@ import tornado
 from tornado.web import Application, RequestHandler, authenticated
 from tornado.websocket import WebSocketHandler
 
+from QUANTAXIS.QASU.user import QA_user_sign_in, QA_user_sign_up
 from QUANTAXIS.QAUtil.QASql import QA_util_sql_mongo_setting
-from QUANTAXIS.QASU.user import QA_user_sign_in,QA_user_sign_up
+
 
 class SignupHandler(RequestHandler):
     def get(self):
-        username=self.get_argument('user',default='admin')
-        password=self.get_argument('password',default='admin')
-        if QA_user_sign_up(username,password,client=QA_util_sql_mongo_setting()):
+        username = self.get_argument('user', default='admin')
+        password = self.get_argument('password', default='admin')
+        if QA_user_sign_up(username, password, client=QA_util_sql_mongo_setting()):
             self.write('SUCCESS')
         else:
             self.write('WRONG')
-
 
 
 class SigninHandler(RequestHandler):
     def get(self):
-        username=self.get_argument('user',default='admin')
-        password=self.get_argument('password',default='admin')
-        res=QA_user_sign_in(username,password,client=QA_util_sql_mongo_setting())
+        username = self.get_argument('user', default='admin')
+        password = self.get_argument('password', default='admin')
+        res = QA_user_sign_in(username, password,
+                              client=QA_util_sql_mongo_setting())
         if res is not None:
             self.write('SUCCESS')
         else:
             self.write('WRONG')
+
+
+if __name__ == '__main__':
+    app = Application(
+        handlers=[
+
+            (r"/user/signin", SigninHandler),
+            (r"/user/signup", SignupHandler)
+        ],
+        debug=True
+    )
+    app.listen(8010)
+    tornado.ioloop.IOLoop.instance().start()
