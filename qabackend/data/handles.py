b6cce36f6ec18c7f38b8244669befe2155bcecba
yutiansut <yutiansut@qq.com>
2018-02-16 21:41:40 +0800

    #commit

--- qabackend/data/handles.py
+++ qabackend/data/handles.py
@@ -5,9 +5,10 @@ import json
 import tornado
 from tornado.web import Application, RequestHandler, authenticated
 from tornado.websocket import WebSocketHandler
-
+from QUANTAXIS.QAUtil.QATransform import QA_util_to_json_from_pandas
 from QUANTAXIS.QAFetch.QAQuery import QA_fetch_stock_day
 from QUANTAXIS.QAFetch.QAQuery_Advance import QA_fetch_stock_day_adv
+import datetime
 
 
 class BaseHandler(RequestHandler):
@@ -17,15 +18,13 @@ class BaseHandler(RequestHandler):
 
 class StockdayHandler(BaseHandler):
     def get(self):
-        print(self.request.arguments)
+        code = self.get_argument('code', default='000001')
+        start = self.get_argument('start', default='2017-01-01')
+        end = self.get_argument('end', default=str(datetime.date.today()))
+        data = QA_util_to_json_from_pandas(
+            QA_fetch_stock_day(code, start, end, format='pd'))
 
-        code = self.request.arguments.get('code', '000001')
-        start = str(self.request.arguments.get('start', '2017-01-01'))
-        end = str(self.request.arguments.get('end', '2017-12-31'))
-        data = QA_fetch_stock_day(code[0].decode(
-            'utf-8'), start, end, format='json')
-
-        self.write({'result': data, 'status': 200})
+        self.write({'result':data})
 
 
 if __name__ == "__main__":
